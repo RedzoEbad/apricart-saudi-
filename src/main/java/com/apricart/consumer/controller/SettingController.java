756a84033356517c6d3060043ececb7ca10c589a
omer khan <komer3870@gmail.com>
2026-04-16 15:52:02 +0500

    Fix setting warehouse API lazy loading handling

--- src/main/java/com/apricart/consumer/controller/SettingController.java
+++ src/main/java/com/apricart/consumer/controller/SettingController.java
@@ -15,6 +15,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import javax.transaction.Transactional;
 import javax.validation.Valid;
 import java.util.Collections;
 import java.util.List;
@@ -24,7 +25,7 @@ import static com.apricart.consumer.security.constants.ResponseMessage.SETTINGS_
 
 @RestController
 @RequestMapping("/v1/auth/open/settings")
-@Api(value = "Setting Controller", tags = {"Setting"})
+@Api(value = "Setting Controller", tags = { "Setting" })
 public class SettingController {
 
     @Autowired
@@ -35,19 +36,22 @@ public class SettingController {
 
     @ApiOperation(value = "Get all settings")
     @GetMapping
-    public ResponseEntity<GenericResponse<List<SettingResponseDTO>>> getAllSettings(@RequestHeader("Language") LanguageType lang) {
+    public ResponseEntity<GenericResponse<List<SettingResponseDTO>>> getAllSettings(
+            @RequestHeader("Language") LanguageType lang) {
         List<SettingResponseDTO> settings = Setting.toDTOList(settingService.getAllSettings(lang));
         return !settings.isEmpty() ? Response.success(settings) : Response.notFound();
     }
+
     @ApiOperation(value = "Get setting by Id")
     @GetMapping("/{id}")
-    public ResponseEntity<GenericResponse<SettingResponseDTO>> findSettingById(@PathVariable Long id, @RequestHeader("Language") LanguageType lang) {
+    public ResponseEntity<GenericResponse<SettingResponseDTO>> findSettingById(@PathVariable Long id,
+            @RequestHeader("Language") LanguageType lang) {
 
         SettingResponseDTO setting = Setting.toDTO(settingService.findById(id, lang));
-        List<FAQResponseDTO> faqsEnglish  = faqService.findBySettingIdAndLanguage(id, LanguageType.ENG);
-        List<FAQResponseDTO> faqsArabic  = faqService.findBySettingIdAndLanguage(id, LanguageType.ARB);
+        List<FAQResponseDTO> faqsEnglish = faqService.findBySettingIdAndLanguage(id, LanguageType.ENG);
+        List<FAQResponseDTO> faqsArabic = faqService.findBySettingIdAndLanguage(id, LanguageType.ARB);
 
-        if(setting != null){
+        if (setting != null) {
             setting.setFaqs(faqsEnglish != null && !faqsEnglish.isEmpty() ? faqsEnglish : Collections.emptyList());
             setting.setArabicFaqs(faqsArabic != null && !faqsArabic.isEmpty() ? faqsArabic : Collections.emptyList());
         }
@@ -55,13 +59,17 @@ public class SettingController {
         return setting != null ? Response.success(setting) : Response.notFound();
     }
 
+    @Transactional
     @ApiOperation(value = "Get setting by warehouseId")
     @GetMapping("/warehouse/{id}")
-    public ResponseEntity<GenericResponse<List<SettingResponseDTO>>> findSettingByWarehouseId(@PathVariable Long id, @RequestHeader("Language") LanguageType lang) {
+    public ResponseEntity<GenericResponse<List<SettingResponseDTO>>> findSettingByWarehouseId(@PathVariable Long id,
+            @RequestHeader("Language") LanguageType lang) {
 
         List<SettingResponseDTO> settings = Setting.toDTOList(settingService.findByWarehouseId(id));
 
-        if (settings == null || settings.isEmpty()) { return Response.notFound(); }
+        if (settings == null || settings.isEmpty()) {
+            return Response.notFound();
+        }
 
         for (SettingResponseDTO setting : settings) {
             List<FAQResponseDTO> faqsEnglish = faqService.findBySettingIdAndLanguage(setting.getId(), LanguageType.ENG);
@@ -76,21 +84,26 @@ public class SettingController {
 
     @ApiOperation(value = "Add Setting")
     @PostMapping
-    public ResponseEntity<GenericResponse<SettingResponseDTO>> addSetting(@Valid @RequestBody SettingRequestDTO settingRequestDTO, @RequestHeader("Language") LanguageType lang) {
+    public ResponseEntity<GenericResponse<SettingResponseDTO>> addSetting(
+            @Valid @RequestBody SettingRequestDTO settingRequestDTO, @RequestHeader("Language") LanguageType lang) {
         settingService.addSetting(settingRequestDTO);
         return Response.created();
     }
 
     @ApiOperation(value = "Update Setting")
     @PutMapping
-    public ResponseEntity<GenericResponse<SettingResponseDTO>> updateSetting(@Valid @RequestBody SettingRequestDTO settingRequestDTO, @RequestHeader("Language") LanguageType lang) {
+    public ResponseEntity<GenericResponse<SettingResponseDTO>> updateSetting(
+            @Valid @RequestBody SettingRequestDTO settingRequestDTO, @RequestHeader("Language") LanguageType lang) {
         SettingResponseDTO updatedSetting = Setting.toDTO(settingService.updateSetting(settingRequestDTO, lang));
         return updatedSetting != null ? Response.success(updatedSetting) : Response.notFound();
     }
+
     @ApiOperation(value = "Delete Setting By Id")
     @DeleteMapping("/{id}")
-    public ResponseEntity<GenericResponse<String>> deleteSetting(@PathVariable Long id, @RequestHeader("Language") LanguageType lang) {
+    public ResponseEntity<GenericResponse<String>> deleteSetting(@PathVariable Long id,
+            @RequestHeader("Language") LanguageType lang) {
         settingService.deleteSetting(id, lang);
-        return lang.equals(LanguageType.ARB) ? Response.success(SETTINGS_REMOVED_SUCCESSFULLY_ARABIC) : Response.success(SETTINGS_REMOVED_SUCCESSFULLY);
+        return lang.equals(LanguageType.ARB) ? Response.success(SETTINGS_REMOVED_SUCCESSFULLY_ARABIC)
+                : Response.success(SETTINGS_REMOVED_SUCCESSFULLY);
     }
 }
