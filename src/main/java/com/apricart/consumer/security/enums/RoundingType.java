88be2b6d059317eaececb82dc741c24c1e3bcf1f
Omer Khan <komer3870@gmail.com>
2026-04-24 19:04:46 +0500

    Add NEAREST_WHOLE_NUMBER to RoundingType enum

--- src/main/java/com/apricart/consumer/security/enums/RoundingType.java
+++ src/main/java/com/apricart/consumer/security/enums/RoundingType.java
@@ -1,5 +1,5 @@
 package com.apricart.consumer.security.enums;
 
 public enum RoundingType {
-    ROUND_TO_DOLLAR_MINUS_01, NO_ROUNDING
+    ROUND_TO_DOLLAR_MINUS_01, NO_ROUNDING, NEAREST_WHOLE_NUMBER
 }
